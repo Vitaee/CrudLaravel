41be3703574034d1d4aa79a090f7815d0f20080d
Can İlgu <canilguu@gmail.com>
2023-02-21 13:13:43 +0300

    Untracked files issue resolved

--- README.md
+++
@@ -1,2 +0,0 @@
-# CrudLaravel
-This project will include docker, aws, postgreSQL, queue services alongside authentication and crud operations for users.
--- init.sql
+++
@@ -1,8 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS dblink;
-
-DO $$
-BEGIN
-PERFORM dblink_exec('', 'CREATE DATABASE laravelcrud_dev');
-EXCEPTION WHEN duplicate_database THEN RAISE NOTICE '%, skipping', SQLERRM USING ERRCODE = SQLSTATE;
-END
-$$;
