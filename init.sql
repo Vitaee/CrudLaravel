fe11ec4f5c95ef4a8a8311de7f6a4941fb021f20
Can İlgu <canilguu@gmail.com>
2023-02-21 16:30:00 +0300

    First test of ci/cd actions

--- init.sql
+++ init.sql
@@ -6,3 +6,10 @@ PERFORM dblink_exec('', 'CREATE DATABASE laravelcrud_dev');
 EXCEPTION WHEN duplicate_database THEN RAISE NOTICE '%, skipping', SQLERRM USING ERRCODE = SQLSTATE;
 END
 $$;
+
+DO $$
+BEGIN
+PERFORM dblink_exec('', 'CREATE DATABASE laravelcrud_test');
+EXCEPTION WHEN duplicate_database THEN RAISE NOTICE '%, skipping', SQLERRM USING ERRCODE = SQLSTATE;
+END
+$$;
